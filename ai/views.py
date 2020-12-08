efa8110af176c82383b79127e10261186da3f116
sunyeongchoi <sn0716@naver.com>
2020-12-08 12:20:02 +0900

    test

--- ai/views.py
+++ ai/views.py
@@ -5,9 +5,14 @@ from rest_framework.views import APIView
 from rest_framework import viewsets, permissions, generics, status, filters
 
 # Create your views here.
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def HelloAPI(request):
     # 이미지 정보 받음
+    param = request.data
+    print('param : ', param)
+    # img_url = param['url']
+    # reg_date = param['reg_date']
+    # reg_time = param['reg_time']
 
     # AI분석 결과
 
@@ -16,4 +21,4 @@ def HelloAPI(request):
     # 빅데이터 함수 호출
 
     # 상태 리턴
-    return Response("hello world!")
+    return Response("hello world!", param)
