d39e9de14f441912ec1832ba2c860b8c62deeeda
sunyeongchoi <sn0716@naver.com>
2020-12-08 10:58:29 +0900

    bigdata폴더 생성

--- ai/views.py
+++ ai/views.py
@@ -7,4 +7,13 @@ from rest_framework import viewsets, permissions, generics, status, filters
 # Create your views here.
 @api_view(['POST'])
 def HelloAPI(request):
+    # 이미지 정보 받음
+
+    # AI분석 결과
+
+    # 결과 저장
+
+    # 빅데이터 함수 호출
+
+    # 상태 리턴
     return Response("hello world!")
