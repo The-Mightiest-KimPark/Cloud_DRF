1fcd8cbcf023944209087fd28f4ff292a9f36f14
sunyeongchoi <sn0716@naver.com>
2020-12-08 15:10:14 +0900

    ai로직 저장함수 완료

--- ai/models.py
+++ ai/models.py
@@ -7,34 +7,19 @@ from django.contrib.auth.models import AbstractUser
 from datetime import datetime
 
 
-# # 자격증 카테고리 예) 정보통신
-# class Category(models.Model):
-#     cat_id = models.IntegerField(primary_key=True) #PK(카테고리PK)
-#     name = models.CharField(max_length=50) #카테고리 이름
-    
-#     class Meta:
-#         db_table = 'CATEGORY'
+# 식재료 
+class Grocery(models.Model):
+    id = models.AutoField(primary_key=True) #PK(카테고리PK)
+    refri_number = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=20, null=True)
+    count = models.IntegerField(blank=True, null=True)
+    reg_date = models.DateField(blank=True, null=True)
+    reg_time = models.DateField(blank=True, null=True)
+    gubun = models.CharField(max_length=5, null=True)
 
-#     def __str__(self):
-#         return self.name
+    class Meta:
+        db_table = 'GROCERY'
 
-# # 자격증 예) 정보처리기사
-# class Certificate(models.Model):
-#     cert_id = models.IntegerField(primary_key=True) #PK(자격증PK)
-#     cat_id = models.ForeignKey(Category, related_name="certificates", on_delete=models.CASCADE) #FK(카테고리PK)
-#     name = models.CharField(max_length=100) #자격증 이름
-#     department = models.CharField(max_length=100) #시행기관
-#     pass_percent = models.FloatField(max_length=50, blank=True, null=True) #합격률
-#     pass_percent_sil = models.FloatField(max_length=50, blank=True, null=True) #합격률
-#     cost = models.CharField(max_length=500, blank=True, null=True) #응시료
-#     cost_sil = models.CharField(max_length=500, blank=True, null=True) #응시료
-#     examinee = models.IntegerField(default=0, blank=True, null=True) #응시자 수
-#     examinee_sil = models.IntegerField(default=0, blank=True, null=True) #응시자 수
-#     link = models.CharField(max_length=500, blank=True, null=True)
-
-#     class Meta:
-#         db_table = "CERTIFICATE"
-
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
--- ai/views.py
+++ ai/views.py
@@ -3,22 +3,41 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets, permissions, generics, status, filters
+from .serializers import GrocerySerializer
 
-# Create your views here.
+# AI 이미지 분석을 통한 결과 저장
 @api_view(['GET','POST'])
 def HelloAPI(request):
     # 이미지 정보 받음
     param = request.data
-    print('param : ', param)
-    img_url = param['url']
+    url = param['url']
     reg_date = param['reg_date']
     reg_time = param['reg_time']
+    refri_number = param['refri_number']
 
-    # AI분석 결과
+    # AI분석 로직
+    ai_result = [{
+        'name' : '바나나',
+        'count' : 3
+    },{
+        'name' : '사과',
+        'count' : 1
+    },{
+        'name' : '고구마',
+        'count' : 2
+    }]
 
     # 결과 저장
+    for result in ai_result:
+        result['refri_number'] = refri_number
+        result['reg_date'] = reg_date
+        result['reg_time'] = reg_time
+        result['gubun'] = 1
 
-    # 빅데이터 함수 호출
+        serializer = GrocerySerializer(data=result)
+        if serializer.is_valid():
+            serializer.save()
+            # 빅데이터 함수 호출(냉장고 번호와 재료들 넘겨줘야함?)
 
-    # 상태 리턴
-    return Response("hello world!", img_url)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
