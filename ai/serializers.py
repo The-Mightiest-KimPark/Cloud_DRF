0d6e456bcc657809334b197f6a3284dcfd064b0c
sunyeongchoi <sn0716@naver.com>
2020-12-10 03:31:14 +0900

    전체 식료품 이름 조회 부분 컬럼 하나만 나오도록 수정

--- ai/serializers.py
+++ ai/serializers.py
@@ -12,3 +12,9 @@ class AllGrocerySerializer(serializers.ModelSerializer):
     class Meta:
         model = AllGrocery
         fields = '__all__'
+
+# 전체 식료품 이름만
+class AllGroceryNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AllGrocery
+        fields = ('name',)
