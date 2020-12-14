0d739566f2d44370f0f6157bf2b777961adcc622
sunyeongchoi <sn0716@naver.com>
2020-12-14 15:25:18 +0900

    ec2test

--- ai/urls.py
+++ ai/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('api/ai-img-grocery/', views.AiImgGrocery),
     path('api/all-grocery-name/', views.AllGroceryName.as_view()),
     path('api/user-input-grocery/', views.userInputGrocery),
-    path('api/test/', views.test),
+    #path('api/test/', views.test),
 ]
