07d51628269df5a15f6c3ceceecffae2ce8dcd68
sunyeongchoi <sn0716@naver.com>
2020-12-16 23:05:48 +0900

    ec2test

--- ai/urls.py
+++ ai/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('api/ai-img-grocery/', views.AiImgGrocery),
+    # path('api/ai-img-grocery/', views.AiImgGrocery),
     path('api/all-grocery-name/', views.AllGroceryName.as_view()),
     path('api/user-input-grocery/', views.userInputGrocery),
     #path('api/test/', views.test),
-    path('api/aibdtest/', views.AiImgGroceryTest)
+    path('api/ai-img-grocery/', views.AiImgGroceryTest)
 ]
